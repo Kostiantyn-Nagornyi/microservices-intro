1766377b35404e9bfa91b96a4956324a53476eba
Kostiantyn_Nagornyi <Kostiantyn_Nagornyi@epam.com>
2025-11-07 15:52:09 +0100

    - Additional "genre" field was introduced to the "song" database-bean - consequently to the respective table. - Minor naming correction was introduces to the db-beans of respective services, in order to get rid of floating bug that prevents database auto-population by Hibernate, despite the auto-population feature was disabled. This is needed for sourcecode consistency.

--- songs-service/src/main/java/com/ms/intro/dto/SongDto.java
+++ songs-service/src/main/java/com/ms/intro/dto/SongDto.java
@@ -1,7 +1,6 @@
 package com.ms.intro.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,6 +16,8 @@ public class SongDto {
     String name;
 
     String artist;
+
+    String genre;
 
     String album;
 
